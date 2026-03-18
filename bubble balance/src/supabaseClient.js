52258cdf2d33681e1ef800a080eafabd4ea88e1d
Juliana <julianamae.saban-24@cpu.edu.ph>
2026-03-18 17:15:18 +0800

    supabaseClient

--- bubble balance/src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.DATABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
