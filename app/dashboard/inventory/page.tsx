83745a47f79b0d689f51ab7b7d1bc6fe420cd5bf
lavefayt <102245874+lavefayt@users.noreply.github.com>
2026-03-31 01:48:56 +0800

    feat: added sidebar and global header with route links

---
+++ app/dashboard/inventory/page.tsx
@@ -0,0 +1,3 @@
+export default function InventoryPage() {
+	return <div className="text-black">Inventory Page</div>;
+}
---
+++ app/dashboard/sales/page.tsx
@@ -0,0 +1,3 @@
+export default function SalesPage() {
+	return <div className="text-black">Sales Page</div>;
+}
